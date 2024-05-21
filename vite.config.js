1d7d9efbea7fe88cfaa2b18f168193c31d042df0
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-05-21 17:51:32 +0200

    tweak build config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,45 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
+import { extname, relative, resolve } from 'path'
+import { fileURLToPath } from 'node:url'
+import { glob } from 'glob'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		libInjectCss(),
+	],
 	build: {
+		copyPublicDir: false,
 		lib: {
 			name: 'EightshiftUiComponents',
-			entry: resolve('src', 'index.js'),
-			formats: ['es'],
-			rollupOptions: {
-				external: ['react', 'react/jsx-runtime'],
-			},
-			manifest: true,
-			copyPublicDir: false,
+			entry: resolve(__dirname, 'lib/index.js'),
+			formats: ['es']
+		},
+		rollupOptions: {
+			external: ['react', 'react/jsx-runtime'],
+			input: Object.fromEntries(
+				// https://rollupjs.org/configuration-options/#input
+				glob.sync('lib/**/*.{js,jsx}', {
+					ignore: ["lib/**/*.d.ts"],
+				}).map(file => [
+					// 1. The name of the entry point
+					// lib/nested/foo.js becomes nested/foo
+					relative(
+						'lib',
+						file.slice(0, file.length - extname(file).length)
+					),
+					// 2. The absolute path to the entry file
+					// lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+					fileURLToPath(new URL(file, import.meta.url))
+				])
+			),
+			output: {
+				assetFileNames: 'assets/[name][extname]',
+				entryFileNames: '[name].js',
+			}
 		}
 	}
 })
