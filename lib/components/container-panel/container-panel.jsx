f16d49dbe15a315feefd2ef5e4d2ebd5a414aa06
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-05-21 18:49:20 +0200

    container panel update

--- lib/components/container-panel/container-panel.jsx
+++ lib/components/container-panel/container-panel.jsx
@@ -1,5 +1,15 @@
 import { classnames } from '../../utilities/classnames';
 
-export const ContainerPanel = ({ children, className }) => {
-	return <div className={classnames('es-uic-p-4 es-uic-space-y-2.5 font-[16px]', className)}>{children}</div>;
+export const ContainerPanel = ({ children, className, title }) => {
+	return (
+		<div
+			className={classnames(
+				'es-uic-space-y-2.5 es-uic-border-t es-uic-border-t-gray-200 es-uic-p-4 es-uic-text-[16px]',
+				className,
+			)}
+		>
+			{title && <span className='es-uic-text-sm es-uic-font-medium es-uic-text-gray-400'>{title}</span>}
+			{children}
+		</div>
+	);
 };
