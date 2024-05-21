1a06df1ab7aa099ff49d8b97e9fb0d82c9b74ef8
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-05-21 13:34:33 +0200

    prepare build setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/index.js'),
+			formats: ['es'],
+			rollupOptions: {
+				external: ['react', 'react/jsx-runtime'],
+			}
+		}
+	}
 })
