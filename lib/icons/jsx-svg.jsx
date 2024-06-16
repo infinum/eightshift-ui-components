3cb68c5329f5dec2f8bbb139c710cebbe7f1b3c7
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-06-16 13:33:29 +0200

    add jsxsvg component

---
+++ lib/icons/jsx-svg.jsx
@@ -0,0 +1,29 @@
+import { svgToJsxString } from 'svg-to-jsx-string';
+import JsxParser from 'react-jsx-parser';
+
+/**
+ * Renders SVG string as JSX SVGs.
+ *
+ * @component
+ * @param {Object} props - Component props.
+ * @param {string} props.svg - The SVG string to render.
+ * @param {string} [props.className] - The class name to apply to the SVG. **Note**: Make sure the SVG doesn't include a class name already!
+ *
+ * @returns {JSX.Element} The JsxSvg component.
+ *
+ * @example
+ * <JsxSvg svg="<svg ..." />
+ *
+ * @preserve
+ */
+export const JsxSvg = (props) => {
+	const { svg, className } = props;
+	const jsxString = svgToJsxString(svg).replace('<svg ', `<svg className="${className}" `);
+
+	return (
+		<JsxParser
+			renderInWrapper={false}
+			jsx={jsxString}
+		/>
+	);
+};
