96d1ebda586276712dd0bef002c99f524d9a7330
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-06-17 00:24:52 +0200

    update file placeholder

--- lib/components/placeholders/file-placeholder.jsx
+++ lib/components/placeholders/file-placeholder.jsx
@@ -10,6 +10,7 @@ import { clsx } from 'clsx/lite';
  * @param {Object} props - Component props.
  * @param {JSX.Element} [props.icon] - File type icon override.
  * @param {string} [props.fileName] - Current file name.
+ * @param {string} [props.className] - Classes to pass to the component.
  *
  * @returns {JSX.Element} The FilePlaceholder component.
  *
@@ -22,15 +23,28 @@ import { clsx } from 'clsx/lite';
  * @preserve
  */
 export const FilePlaceholder = (props) => {
-	const { icon, fileName } = props;
+	const { icon, fileName, className, children } = props;
+
+	const commonClassName = clsx(
+		'es-uic-w-fit es-uic-rounded-lg es-uic-border es-uic-border-gray-300 es-uic-bg-gray-50 es-uic-p-2 es-uic-pr-3 es-uic-text-xs es-uic-text-gray-300 es-uic-shadow es-uic-font-mono',
+		className,
+	);
+
+	if (!fileName) {
+		return (
+			<RichLabel
+				icon={icon ?? icons.file}
+				label={children ?? __('No file selected', 'eightshift-ui-components')}
+				className={commonClassName}
+			/>
+		);
+	}
+
 	return (
 		<RichLabel
 			icon={icon ?? icons.file}
 			label={fileName ?? __('No file selected', 'eightshift-ui-components')}
-			className={clsx(
-				'es-uic-w-fit es-uic-rounded-lg es-uic-border es-uic-border-gray-300 es-uic-bg-gray-50 es-uic-p-2 es-uic-pr-3 es-uic-text-xs es-uic-text-gray-300 es-uic-shadow',
-				fileName && 'es-uic-font-mono',
-			)}
+			className={commonClassName}
 		/>
 	);
 };
