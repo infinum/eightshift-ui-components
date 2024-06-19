15aa01d36c36de4ceb4093d8a3fe7f8fcdbac634
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-06-19 21:29:56 +0200

    tweak richlabel

--- lib/components/rich-label/rich-label.jsx
+++ lib/components/rich-label/rich-label.jsx
@@ -13,6 +13,7 @@ import { clsx } from 'clsx/lite';
  * @param {boolean} [props.fullWidth=false] - If `true`, the component will take up as much space as it can.
  * @param {boolean} [props.contentsOnly] - If `true`, only the label (/icon/subtitle) will be rendered, without any wrapping elements. Useful if you want to provide your own layout.
  * @param {boolean} [props.hidden] - If `true`, the component is not rendered.
+ * @param {boolean} [props.noColor] - If `true`, colors on text won't be set, opacity will be used instead.
  *
  * @returns {JSX.Element} The RichLabel component.
  *
@@ -25,7 +26,7 @@ import { clsx } from 'clsx/lite';
  * @preserve
  */
 export const RichLabel = (props) => {
-	const { icon, label, subtitle, as, className, fullWidth = false, contentsOnly, hidden } = props;
+	const { icon, label, subtitle, as, className, fullWidth = false, contentsOnly, hidden, noColor } = props;
 
 	if (hidden) {
 		return null;
@@ -36,10 +37,14 @@ export const RichLabel = (props) => {
 	if (contentsOnly) {
 		return (
 			<>
-				{icon && <span className='es-uic-text-slate-500 [&>svg]:es-uic-size-5.5'>{icon}</span>}
-				{label && <span className='es-uic-text-balance'>{label}</span>}
+				{icon && <span className={clsx('[&>svg]:es-uic-size-5.5', !noColor && 'es-uic-text-slate-500')}>{icon}</span>}
+				{label && <span className={clsx('es-uic-text-balance', !noColor && 'es-uic-text-gray-800')}>{label}</span>}
 				{subtitle && (
-					<span className='es-uic-text-balance es-uic-text-xs es-uic-text-gray-500 es-uic-opacity-60'>{subtitle}</span>
+					<span
+						className={clsx('es-uic-text-balance es-uic-text-xs es-uic-opacity-60', !noColor && 'es-uic-text-gray-800')}
+					>
+						{subtitle}
+					</span>
 				)}
 			</>
 		);
@@ -48,7 +53,9 @@ export const RichLabel = (props) => {
 	return (
 		<ComponentToRender
 			className={clsx(
-				'es-uic-flex es-uic-items-center es-uic-gap-1.5 es-uic-text-sm es-uic-text-gray-800 [&>span>svg]:es-uic-text-slate-500',
+				'es-uic-flex es-uic-items-center es-uic-gap-1.5 es-uic-text-sm',
+				!noColor && 'es-uic-text-gray-800 [&>span>svg]:es-uic-text-slate-500',
+				noColor && '[&>span>svg]:es-uic-opacity-80',
 				fullWidth && 'es-uic-grow',
 				className,
 			)}
