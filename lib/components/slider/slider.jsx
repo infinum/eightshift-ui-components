f02fb6d9849b455f0b298c7de791bd740295b78b
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-07-04 18:04:23 +0200

    fixed slider containment when label is not present

--- lib/components/slider/slider.jsx
+++ lib/components/slider/slider.jsx
@@ -162,204 +162,207 @@ export const Slider = (props) => {
 				}
 				labelAs={Label}
 				className={labelClassName}
-				controlContainerClassName={clsx('es-uic-flex es-uic-items-center es-uic-gap-2.5 !es-uic-space-y-0', vertical && 'es-uic-flex-col')}
 			>
-				{before && <div className='es-uic-flex es-uic-shrink-0 es-uic-items-center es-uic-gap-1'>{before}</div>}
+				<div className={clsx('es-uic-flex es-uic-items-center es-uic-gap-2.5 !es-uic-space-y-0', vertical && 'es-uic-flex-col')}>
+					{before && <div className='es-uic-flex es-uic-shrink-0 es-uic-items-center es-uic-gap-1'>{before}</div>}
 
-				<ReactAriaSliderTrack
-					className={clsx(
-						'es-uic-isolate es-uic-grid es-uic-grid-cols-1 es-uic-grid-rows-1',
-						vertical ? 'es-uic-mx-auto es-uic-h-40 es-uic-w-4' : 'es-uic-h-4 es-uic-w-full es-uic-grow',
-					)}
-				>
-					{({ state }) => {
-						let activeBarLeft;
-						let activeBarBottom;
-						let activeBarWidth;
-						let activeBarHeight;
+					<ReactAriaSliderTrack
+						className={clsx(
+							'es-uic-isolate es-uic-grid es-uic-grid-cols-1 es-uic-grid-rows-1',
+							vertical ? 'es-uic-mx-auto es-uic-h-40 es-uic-w-4' : 'es-uic-h-4 es-uic-w-full es-uic-grow',
+						)}
+					>
+						{({ state }) => {
+							let activeBarLeft;
+							let activeBarBottom;
+							let activeBarWidth;
+							let activeBarHeight;
 
-						let activeBarOffset = false;
-						let shouldRoundStart = false;
-						let shouldRoundEnd = false;
+							let activeBarOffset = false;
+							let shouldRoundStart = false;
+							let shouldRoundEnd = false;
 
-						const currValue = state.getThumbValue(0);
+							const currValue = state.getThumbValue(0);
 
-						if (vertical && !noActiveHighlight) {
-							if (Array.isArray(value)) {
-								activeBarBottom = state.getThumbPercent(0) * 100;
-								activeBarHeight = state.getThumbPercent(value.length - 1) * 100 - activeBarBottom;
-							} else if (startPoint && currValue >= startPoint) {
-								activeBarBottom = state.getValuePercent(startPoint) * 100;
-								activeBarHeight = state.getThumbPercent(0) * 100 - activeBarBottom;
-								activeBarOffset = true;
-							} else if (startPoint && currValue < startPoint) {
-								activeBarBottom = state.getThumbPercent(0) * 100;
-								activeBarHeight = state.getValuePercent(startPoint) * 100 - activeBarBottom;
-							} else if (min < 0 && currValue >= 0) {
-								activeBarBottom = state.getValuePercent(0) * 100;
-								activeBarHeight = state.getThumbPercent(0) * 100 - activeBarBottom;
-							} else if (min < 0 && currValue < 0) {
-								activeBarBottom = state.getThumbPercent(0) * 100;
-								activeBarHeight = state.getValuePercent(0) * 100 - activeBarBottom;
-							} else {
-								activeBarBottom = state.getValuePercent(min) * 100;
-								activeBarHeight = state.getThumbPercent(0) * 100;
-								shouldRoundEnd = true;
+							if (vertical && !noActiveHighlight) {
+								if (Array.isArray(value)) {
+									activeBarBottom = state.getThumbPercent(0) * 100;
+									activeBarHeight = state.getThumbPercent(value.length - 1) * 100 - activeBarBottom;
+								} else if (startPoint && currValue >= startPoint) {
+									activeBarBottom = state.getValuePercent(startPoint) * 100;
+									activeBarHeight = state.getThumbPercent(0) * 100 - activeBarBottom;
+									activeBarOffset = true;
+								} else if (startPoint && currValue < startPoint) {
+									activeBarBottom = state.getThumbPercent(0) * 100;
+									activeBarHeight = state.getValuePercent(startPoint) * 100 - activeBarBottom;
+								} else if (min < 0 && currValue >= 0) {
+									activeBarBottom = state.getValuePercent(0) * 100;
+									activeBarHeight = state.getThumbPercent(0) * 100 - activeBarBottom;
+								} else if (min < 0 && currValue < 0) {
+									activeBarBottom = state.getThumbPercent(0) * 100;
+									activeBarHeight = state.getValuePercent(0) * 100 - activeBarBottom;
+								} else {
+									activeBarBottom = state.getValuePercent(min) * 100;
+									activeBarHeight = state.getThumbPercent(0) * 100;
+									shouldRoundEnd = true;
+								}
+							} else if (!noActiveHighlight) {
+								if (Array.isArray(value)) {
+									activeBarLeft = state.getThumbPercent(0) * 100;
+									activeBarWidth = state.getThumbPercent(value.length - 1) * 100 - activeBarLeft;
+								} else if (startPoint && currValue >= startPoint) {
+									activeBarLeft = state.getValuePercent(startPoint) * 100;
+									activeBarWidth = state.getThumbPercent(0) * 100 - activeBarLeft;
+								} else if (startPoint && currValue < startPoint) {
+									activeBarLeft = state.getThumbPercent(0) * 100;
+									activeBarWidth = state.getValuePercent(startPoint) * 100 - activeBarLeft;
+								} else if (min < 0 && currValue >= 0) {
+									activeBarLeft = state.getValuePercent(0) * 100;
+									activeBarWidth = state.getThumbPercent(0) * 100 - activeBarLeft;
+									activeBarOffset = true;
+								} else if (min < 0 && currValue < 0) {
+									activeBarLeft = state.getThumbPercent(0) * 100;
+									activeBarWidth = state.getValuePercent(0) * 100 - activeBarLeft;
+								} else {
+									activeBarLeft = state.getValuePercent(min) * 100;
+									activeBarWidth = state.getThumbPercent(0) * 100;
+									shouldRoundStart = true;
+								}
 							}
-						} else if (!noActiveHighlight) {
-							if (Array.isArray(value)) {
-								activeBarLeft = state.getThumbPercent(0) * 100;
-								activeBarWidth = state.getThumbPercent(value.length - 1) * 100 - activeBarLeft;
-							} else if (startPoint && currValue >= startPoint) {
-								activeBarLeft = state.getValuePercent(startPoint) * 100;
-								activeBarWidth = state.getThumbPercent(0) * 100 - activeBarLeft;
-							} else if (startPoint && currValue < startPoint) {
-								activeBarLeft = state.getThumbPercent(0) * 100;
-								activeBarWidth = state.getValuePercent(startPoint) * 100 - activeBarLeft;
-							} else if (min < 0 && currValue >= 0) {
-								activeBarLeft = state.getValuePercent(0) * 100;
-								activeBarWidth = state.getThumbPercent(0) * 100 - activeBarLeft;
-								activeBarOffset = true;
-							} else if (min < 0 && currValue < 0) {
-								activeBarLeft = state.getThumbPercent(0) * 100;
-								activeBarWidth = state.getValuePercent(0) * 100 - activeBarLeft;
-							} else {
-								activeBarLeft = state.getValuePercent(min) * 100;
-								activeBarWidth = state.getThumbPercent(0) * 100;
-								shouldRoundStart = true;
-							}
-						}
 
-						return (
-							<>
-								<div
-									className={clsx(
-										'es-uic-relative es-uic-col-start-1 es-uic-row-start-1 es-uic-rounded-full es-uic-border',
-										!vertical && 'es-uic-h-1.5 es-uic-w-full es-uic-self-center',
-										vertical && 'es-uic-h-full es-uic-w-1.5 es-uic-flex-col es-uic-justify-self-center',
-										disabled ? 'es-uic-border-gray-200 es-uic-bg-white' : 'es-uic-border-gray-300 es-uic-bg-gray-50 es-uic-shadow-sm',
-									)}
-									style={trackStyle}
-								/>
-
-								{!noActiveHighlight && (
+							return (
+								<>
 									<div
 										className={clsx(
-											'es-uic-absolute es-uic-col-start-1 es-uic-row-start-1 es-uic-border',
+											'es-uic-relative es-uic-col-start-1 es-uic-row-start-1 es-uic-rounded-full es-uic-border',
 											!vertical && 'es-uic-h-1.5 es-uic-w-full es-uic-self-center',
 											vertical && 'es-uic-h-full es-uic-w-1.5 es-uic-flex-col es-uic-justify-self-center',
-											!vertical && shouldRoundStart && 'es-uic-rounded-l-full',
-											!vertical && shouldRoundEnd && 'es-uic-rounded-r-full',
-											vertical && shouldRoundStart && 'es-uic-rounded-t-full',
-											vertical && shouldRoundEnd && 'es-uic-rounded-b-full',
-											!vertical && activeBarOffset && '-es-uic-translate-x-px',
-											vertical && activeBarOffset && 'es-uic-translate-y-px',
-											disabled ? 'es-uic-border-gray-200 es-uic-bg-gray-50' : 'es-uic-border-teal-500 es-uic-bg-teal-500/30',
+											disabled ? 'es-uic-border-gray-200 es-uic-bg-white' : 'es-uic-border-gray-300 es-uic-bg-gray-50 es-uic-shadow-sm',
 										)}
-										style={{
-											bottom: vertical ? `${activeBarBottom}%` : null,
-											height: vertical ? `${activeBarHeight}%` : null,
-											width: vertical ? null : `${activeBarWidth}%`,
-											left: vertical ? null : `${activeBarLeft}%`,
-										}}
+										style={trackStyle}
 									/>
-								)}
 
-								{markers && (
-									<div
-										className={clsx(
-											'es-uic-relative es-uic-col-start-1 es-uic-row-start-1',
-											!vertical && 'es-uic-h-1 es-uic-w-full es-uic-self-center',
-											vertical && 'es-uic-h-full es-uic-w-1 es-uic-flex-col es-uic-justify-self-center',
-										)}
-									>
-										{markerData.map(([rawDotValue, labelData], index) => {
-											const dotValue = parseFloat(rawDotValue);
-
-											let isWithinActiveBar = false;
-
-											if (Array.isArray(value)) {
-												isWithinActiveBar = value[0] <= dotValue && dotValue <= value[value.length - 1];
-											} else if (startPoint && dotValue >= startPoint) {
-												isWithinActiveBar = dotValue >= startPoint && dotValue <= currValue && dotValue !== startPoint;
-											} else if (startPoint && dotValue < startPoint) {
-												isWithinActiveBar = dotValue <= startPoint && dotValue >= currValue && dotValue !== startPoint;
-											} else if (min < 0 && dotValue >= 0) {
-												isWithinActiveBar = dotValue >= 0 && dotValue <= currValue && dotValue !== 0;
-											} else if (min < 0 && dotValue < 0) {
-												isWithinActiveBar = dotValue <= 0 && dotValue >= currValue && dotValue !== 0;
-											} else {
-												isWithinActiveBar = dotValue <= currValue && dotValue > min && dotValue < max;
-											}
-
-											return (
-												<div
-													key={index}
-													className={clsx(
-														'es-uic-absolute',
-														vertical ? 'es-uic-h-px es-uic-w-1' : 'es-uic-h-1 es-uic-w-px es-uic-translate-x-1/2',
-														!(
-															dotValue === min ||
-															dotValue === max ||
-															(dotValue === startPoint && !noActiveHighlight) ||
-															(min < 0 && dotValue === 0 && !noActiveHighlight) ||
-															isWithinActiveBar
-														) && 'es-uic-bg-gray-300',
-														!noActiveHighlight && isWithinActiveBar && 'es-uic-bg-teal-500/75',
-													)}
-													style={{
-														left: vertical ? null : `${state.getValuePercent(dotValue) * 100}%`,
-														top: vertical ? `calc(${state.getValuePercent(dotValue) * 100}%)` : null,
-													}}
-												>
-													<span
-														className={clsx(
-															'es-uic-absolute es-uic-select-none es-uic-text-xs es-uic-transition-colors',
-															vertical ? 'es-uic-left-3.5 es-uic-top-0 -es-uic-translate-y-1/2' : 'es-uic-left-0 es-uic-top-2.5 -es-uic-translate-x-1/2',
-															(Array.isArray(value) ? value.includes(Number(dotValue)) : value === Number(dotValue) && !disabled) ? 'es-uic-text-teal-700' : 'es-uic-text-gray-300',
-														)}
-													>
-														{markers && markers !== 'dots' && labelData}
-													</span>
-												</div>
-											);
-										})}
-									</div>
-								)}
-
-								<div className='es-uic-relative es-uic-col-start-1 es-uic-row-start-1 es-uic-p-px'>
-									{state.values.map((_, i) => (
-										<ReactAriaSliderThumb
-											key={i}
-											index={i}
-											aria-label={thumbLabels?.[i]}
+									{!noActiveHighlight && (
+										<div
 											className={clsx(
-												'es-uic-absolute es-uic-size-3.5 es-uic-rounded-full es-uic-border es-uic-transition es-uic-duration-300',
-												vertical ? '!-es-uic-translate-y-1/2 !es-uic-translate-x-0' : '!-es-uic-translate-x-1/2 !es-uic-translate-y-0',
-												'es-uic-shadow dragging:es-uic-bg-teal-600 disabled:es-uic-border-gray-200 disabled:es-uic-bg-gray-100 disabled:es-uic-shadow-none',
-												'focus-visible:es-uic-ring focus-visible:es-uic-ring-teal-500 focus-visible:es-uic-ring-opacity-50',
-												'es-uic-border-teal-600 es-uic-bg-teal-500 es-uic-shadow-teal-600/50',
+												'es-uic-absolute es-uic-col-start-1 es-uic-row-start-1 es-uic-border',
+												!vertical && 'es-uic-h-1.5 es-uic-w-full es-uic-self-center',
+												vertical && 'es-uic-h-full es-uic-w-1.5 es-uic-flex-col es-uic-justify-self-center',
+												!vertical && shouldRoundStart && 'es-uic-rounded-l-full',
+												!vertical && shouldRoundEnd && 'es-uic-rounded-r-full',
+												vertical && shouldRoundStart && 'es-uic-rounded-t-full',
+												vertical && shouldRoundEnd && 'es-uic-rounded-b-full',
+												!vertical && activeBarOffset && '-es-uic-translate-x-px',
+												vertical && activeBarOffset && 'es-uic-translate-y-px',
+												disabled ? 'es-uic-border-gray-200 es-uic-bg-gray-50' : 'es-uic-border-teal-500 es-uic-bg-teal-500/30',
 											)}
-											onFocus={() => {
-												if (state.values.length < 2) {
-													return;
+											style={{
+												bottom: vertical ? `${activeBarBottom}%` : null,
+												height: vertical ? `${activeBarHeight}%` : null,
+												width: vertical ? null : `${activeBarWidth}%`,
+												left: vertical ? null : `${activeBarLeft}%`,
+											}}
+										/>
+									)}
+
+									{markers && (
+										<div
+											className={clsx(
+												'es-uic-relative es-uic-col-start-1 es-uic-row-start-1',
+												!vertical && 'es-uic-h-1 es-uic-w-full es-uic-self-center',
+												vertical && 'es-uic-h-full es-uic-w-1 es-uic-flex-col es-uic-justify-self-center',
+											)}
+										>
+											{markerData.map(([rawDotValue, labelData], index) => {
+												const dotValue = parseFloat(rawDotValue);
+
+												let isWithinActiveBar = false;
+
+												if (Array.isArray(value)) {
+													isWithinActiveBar = value[0] <= dotValue && dotValue <= value[value.length - 1];
+												} else if (startPoint && dotValue >= startPoint) {
+													isWithinActiveBar = dotValue >= startPoint && dotValue <= currValue && dotValue !== startPoint;
+												} else if (startPoint && dotValue < startPoint) {
+													isWithinActiveBar = dotValue <= startPoint && dotValue >= currValue && dotValue !== startPoint;
+												} else if (min < 0 && dotValue >= 0) {
+													isWithinActiveBar = dotValue >= 0 && dotValue <= currValue && dotValue !== 0;
+												} else if (min < 0 && dotValue < 0) {
+													isWithinActiveBar = dotValue <= 0 && dotValue >= currValue && dotValue !== 0;
+												} else {
+													isWithinActiveBar = dotValue <= currValue && dotValue > min && dotValue < max;
 												}
 
-												setCurrentThumbIndex(i);
-											}}
-										>
-											{inputField && state.values.length > 1 && currentThumbIndex === i && <div className='es-uic-m-0.5 es-uic-size-2 es-uic-rounded-full es-uic-bg-teal-100' />}
+												return (
+													<div
+														key={index}
+														className={clsx(
+															'es-uic-absolute',
+															vertical ? 'es-uic-h-px es-uic-w-1' : 'es-uic-h-1 es-uic-w-px es-uic-translate-x-1/2',
+															!(
+																dotValue === min ||
+																dotValue === max ||
+																(dotValue === startPoint && !noActiveHighlight) ||
+																(min < 0 && dotValue === 0 && !noActiveHighlight) ||
+																isWithinActiveBar
+															) && 'es-uic-bg-gray-300',
+															!noActiveHighlight && isWithinActiveBar && 'es-uic-bg-teal-500/75',
+														)}
+														style={{
+															left: vertical ? null : `${state.getValuePercent(dotValue) * 100}%`,
+															top: vertical ? `calc(${state.getValuePercent(dotValue) * 100}%)` : null,
+														}}
+													>
+														<span
+															className={clsx(
+																'es-uic-absolute es-uic-select-none es-uic-text-xs es-uic-transition-colors',
+																vertical ? 'es-uic-left-3.5 es-uic-top-0 -es-uic-translate-y-1/2' : 'es-uic-left-0 es-uic-top-2.5 -es-uic-translate-x-1/2',
+																(Array.isArray(value) ? value.includes(Number(dotValue)) : value === Number(dotValue) && !disabled)
+																	? 'es-uic-text-teal-700'
+																	: 'es-uic-text-gray-300',
+															)}
+														>
+															{markers && markers !== 'dots' && labelData}
+														</span>
+													</div>
+												);
+											})}
+										</div>
+									)}
 
-											{thumbContent && thumbContent(i)}
-										</ReactAriaSliderThumb>
-									))}
-								</div>
-							</>
-						);
-					}}
-				</ReactAriaSliderTrack>
+									<div className='es-uic-relative es-uic-col-start-1 es-uic-row-start-1 es-uic-p-px'>
+										{state.values.map((_, i) => (
+											<ReactAriaSliderThumb
+												key={i}
+												index={i}
+												aria-label={thumbLabels?.[i]}
+												className={clsx(
+													'es-uic-absolute es-uic-size-3.5 es-uic-rounded-full es-uic-border es-uic-transition es-uic-duration-300',
+													vertical ? '!-es-uic-translate-y-1/2 !es-uic-translate-x-0' : '!-es-uic-translate-x-1/2 !es-uic-translate-y-0',
+													'es-uic-shadow dragging:es-uic-bg-teal-600 disabled:es-uic-border-gray-200 disabled:es-uic-bg-gray-100 disabled:es-uic-shadow-none',
+													'focus-visible:es-uic-ring focus-visible:es-uic-ring-teal-500 focus-visible:es-uic-ring-opacity-50',
+													'es-uic-border-teal-600 es-uic-bg-teal-500 es-uic-shadow-teal-600/50',
+												)}
+												onFocus={() => {
+													if (state.values.length < 2) {
+														return;
+													}
 
-				{after && <div className='es-uic-flex es-uic-shrink-0 es-uic-items-center es-uic-gap-1'>{after}</div>}
+													setCurrentThumbIndex(i);
+												}}
+											>
+												{inputField && state.values.length > 1 && currentThumbIndex === i && <div className='es-uic-m-0.5 es-uic-size-2 es-uic-rounded-full es-uic-bg-teal-100' />}
+
+												{thumbContent && thumbContent(i)}
+											</ReactAriaSliderThumb>
+										))}
+									</div>
+								</>
+							);
+						}}
+					</ReactAriaSliderTrack>
+
+					{after && <div className='es-uic-flex es-uic-shrink-0 es-uic-items-center es-uic-gap-1'>{after}</div>}
+				</div>
 			</BaseControl>
 		</ReactAriaSlider>
 	);
