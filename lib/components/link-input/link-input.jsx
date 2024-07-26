53561af04f87a1c2616856e4439ff37b87b4636f
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-07-26 15:25:07 +0200

    rework linkinput

--- lib/components/link-input/link-input.jsx
+++ lib/components/link-input/link-input.jsx
@@ -1,17 +1,15 @@
-import { SearchField, Label, Button as ReactAriaButton, Input, Group, Toolbar } from 'react-aria-components';
+import { Label, Button as ReactAriaButton, Input, Group, ListBox, ListBoxItem, Popover } from 'react-aria-components';
 import { __ } from '@wordpress/i18n';
 import { icons } from '../../icons/icons';
 import { clsx } from 'clsx/lite';
-import { useRef, useState } from 'react';
+import { useRef, cloneElement } from 'react';
 import { useAsyncList } from 'react-stately';
 import { Spacer } from '../spacer/spacer';
 import { Tooltip } from '../tooltip/tooltip';
 import { AnimatedVisibility } from '../animated-visibility/animated-visibility';
 import { BaseControl } from '../base-control/base-control';
-import { Button } from '../button/button';
 import { RichLabel } from '../rich-label/rich-label';
-import { Popover } from '../popover/popover';
-import { useDebouncedCallback } from 'use-debounce';
+import { ComboBox } from 'react-aria-components';
 
 /**
  * Component that allows URL selection, with a suggestionList of suggestions and type-to-search.
@@ -66,21 +64,15 @@ export const LinkInput = (props) => {
 		fetchSuggestions,
 		className,
 
-		inputDebounceDelay = 500,
-
 		hidden,
 	} = props;
 
-	const [inputValue, setInputValue] = useState(url ?? '');
-	const [suggestionsVisible, setSuggestionsVisible] = useState(false);
-
 	const triggerRef = useRef(null);
-	const inputRef = useRef(null);
 
 	const suggestionList = useAsyncList({
-		getKey: ({ item }) => item.value,
-		async load({ signal }) {
-			const items = await fetchSuggestions(inputValue, signal);
+		async load({ signal, filterText }) {
+			// const items = await fetchSuggestions(inputValue, signal);
+			const items = await fetchSuggestions(filterText, signal);
 
 			return {
 				items: items ?? [],
@@ -88,239 +80,203 @@ export const LinkInput = (props) => {
 		},
 	});
 
-	const shouldNotShowSuggestions = (url) => {
+	const shouldShowSuggestions = (() => {
 		if (!url) {
-			return true;
+			return false;
 		}
 
-		return (
-			url?.trim()?.length < 3 ||
+		return !(
+			url?.trim()?.length < 4 ||
 			url?.startsWith('#') ||
 			url?.startsWith(':') ||
-			url?.startsWith('mailto:') ||
-			url?.startsWith('tel:') ||
-			url?.startsWith('http://') ||
-			url?.startsWith('https://') ||
-			url?.startsWith('www.')
+			url?.startsWith('mailto') ||
+			url?.startsWith('tel') ||
+			url?.startsWith('http') ||
+			url?.startsWith('www')
 		);
-	};
-
-	const debounced = useDebouncedCallback(
-		(value) => {
-			onChange({ url: value, isAnchor: value?.includes('#') });
-
-			if (shouldNotShowSuggestions(value)) {
-				setSuggestionsVisible(false);
-			} else {
-				suggestionList.setFilterText(value);
-				setSuggestionsVisible(value?.length > 3);
-			}
-		},
-		inputDebounceDelay,
-		{ maxWait: 2000 },
-	);
+	})();
 
 	if (hidden) {
 		return null;
 	}
 
 	return (
-		<>
-			<SearchField
-				className='es-uic-w-full es-uic-space-y-1'
-				type='url'
-				value={inputValue}
-				onChange={(value) => {
-					setInputValue(value);
-					debounced(value);
-				}}
-				isDisabled={disabled}
+		<ComboBox
+			items={suggestionList.items}
+			inputValue={suggestionList.filterText}
+			onInputChange={(value) => {
+				onChange({ url: value, isAnchor: value?.includes('#') });
+				suggestionList.setFilterText(value);
+			}}
+			allowsCustomValue
+			allowsEmptyCollection={shouldShowSuggestions}
+			isDisabled={disabled}
+		>
+			<BaseControl
+				as={Label}
+				icon={icon}
+				label={label}
+				subtitle={subtitle}
+				actions={actions}
+				labelAs={Label}
+				help={help}
 			>
-				<BaseControl
-					as={Label}
-					icon={icon}
-					label={label}
-					subtitle={subtitle}
-					actions={actions}
-					labelAs={Label}
-					help={help}
+				<Group
+					className='es-uic-relative'
+					ref={triggerRef}
 				>
-					<Group
-						className='es-uic-relative'
-						ref={triggerRef}
+					<Input
+						placeholder={placeholder}
+						className={clsx(
+							'es-uic-min-h-10 es-uic-w-full es-uic-rounded-md es-uic-border es-uic-border-gray-300 es-uic-py-2 es-uic-pl-2 es-uic-pr-1 es-uic-text-sm es-uic-shadow-sm es-uic-transition selection:es-uic-bg-teal-500/20 selection:es-uic-text-teal-950',
+							'focus:es-uic-outline-none focus-visible:es-uic-outline-none focus-visible:es-uic-ring focus-visible:es-uic-ring-teal-500 focus-visible:es-uic-ring-opacity-50',
+							url?.length > 0 && 'es-uic-pr-8',
+							className,
+						)}
+					/>
+
+					<AnimatedVisibility
+						visible={url?.length > 0}
+						className='es-uic-absolute es-uic-inset-y-1 es-uic-right-1'
+						transition='fade'
 					>
-						<Input
-							placeholder={placeholder}
-							ref={inputRef}
-							className={clsx(
-								'es-uic-min-h-10 es-uic-w-full es-uic-rounded-md es-uic-border es-uic-border-gray-300 es-uic-py-2 es-uic-pl-2 es-uic-pr-1 es-uic-text-sm es-uic-shadow-sm es-uic-transition selection:es-uic-bg-teal-500/20 selection:es-uic-text-teal-950',
-								'focus:es-uic-outline-none focus-visible:es-uic-outline-none focus-visible:es-uic-ring focus-visible:es-uic-ring-teal-500 focus-visible:es-uic-ring-opacity-50',
-								inputValue?.length > 0 && 'es-uic-pr-8',
-								className,
-							)}
-							onFocus={(e) => {
-								onChange({ url: inputValue, isAnchor: inputValue?.includes('#') });
-
-								if (props.onFocus) {
-									props.onFocus(e);
-								}
+						<ReactAriaButton
+							slot={null}
+							onPress={() => {
+								suggestionList.setFilterText('');
+								onChange({ url: undefined, isAnchor: false });
 							}}
-						/>
-
-						<AnimatedVisibility
-							visible={inputValue?.length > 0}
-							className='es-uic-absolute es-uic-inset-y-1 es-uic-right-1'
-							transition='scaleFade'
+							className='focus:es-uic-outline-none'
 						>
-							<ReactAriaButton>
-								<Tooltip text={__('Clear', 'eightshift-ui-components')}>
-									<div
-										className={clsx(
-											'es-uic-flex es-uic-size-8 es-uic-items-center es-uic-justify-center es-uic-rounded es-uic-bg-white/85 es-uic-text-gray-600 es-uic-backdrop-blur es-uic-transition',
-											'hover:es-uic-bg-red-50 hover:es-uic-text-red-500',
-											'[&>svg]:es-uic-size-6',
-										)}
-									>
-										{removeIcon}
-									</div>
-								</Tooltip>
-							</ReactAriaButton>
-						</AnimatedVisibility>
-					</Group>
-				</BaseControl>
-			</SearchField>
+							<Tooltip text={__('Clear', 'eightshift-ui-components')}>
+								<div
+									className={clsx(
+										'es-uic-flex es-uic-size-8 es-uic-items-center es-uic-justify-center es-uic-rounded es-uic-bg-white/85 es-uic-text-gray-600 es-uic-backdrop-blur es-uic-transition',
+										'hover:es-uic-bg-red-50 hover:es-uic-text-red-500',
+										'[&>svg]:es-uic-size-6',
+									)}
+								>
+									{removeIcon}
+								</div>
+							</Tooltip>
+						</ReactAriaButton>
+					</AnimatedVisibility>
+				</Group>
+			</BaseControl>
 
 			<Popover
 				aria-label={__('URL suggestions', 'eightshift-ui-components')}
-				shouldFlip={false}
-				triggerRef={triggerRef}
-				isOpen={suggestionsVisible}
-				onOpenChange={(value) => {
-					setSuggestionsVisible(value);
-					debounced.cancel();
-				}}
-				placement='bottom'
-				className='!es-uic-p-0'
+				className={({ isEntering, isExiting }) =>
+					clsx(
+						'es-uic-rounded-md es-uic-border es-uic-border-gray-200 es-uic-bg-white es-uic-shadow-lg es-uic-outline-none empty:es-uic-opacity-0',
+						isEntering && 'es-uic-outline es-uic-animate-in es-uic-fade-in-0 es-uic-slide-in-from-top-2 es-uic-fill-mode-forwards',
+						isExiting && 'es-uic-outline es-uic-animate-out es-uic-fade-out-0 es-uic-slide-out-to-top-2 es-uic-fill-mode-forwards',
+					)
+				}
 				style={{
 					width: `${triggerRef.current?.offsetWidth}px`,
 				}}
 			>
 				{suggestionList.isLoading && (
 					<RichLabel
-						icon={icons.progressbarIntermittent}
-						label={__('Loading suggestions...', 'eightshift-ui-components')}
-						subtitle={__('Please wait', 'eightshift-ui-components')}
-						className='es-uic-min-h-14 es-uic-p-2'
+						icon={cloneElement(icons.emptyCircle, { className: 'es-uic-animate-spin' })}
+						label={__('Loading suggestions', 'eightshift-ui-components')}
+						className='es-uic-min-h-12 es-uic-p-2'
 					/>
 				)}
 
 				{!suggestionList.isLoading && suggestionList.items.length === 0 && (
 					<RichLabel
 						icon={icons.searchEmpty}
-						label={__('No suggestions found', 'eightshift-ui-components')}
+						label={__('No results', 'eightshift-ui-components')}
 						subtitle={__('Try a different search term.', 'eightshift-ui-components')}
-						className='es-uic-min-h-14 es-uic-p-2'
+						className='es-uic-min-h-12 es-uic-p-2'
 					/>
 				)}
 
 				{!suggestionList.isLoading && suggestionList.items.length > 0 && (
-					<Toolbar
-						orientation='vertical'
-						className='es-uic-flex es-uic-min-h-10 es-uic-flex-col es-uic-space-y-1 es-uic-p-2'
-					>
-						{suggestionList.items.map((item, index) => {
-							const {
-								label: title,
-								value: url,
-								metadata: { subtype: rawSubtype },
-							} = item;
+					<>
+						<ListBox className='es-uic-space-y-1 es-uic-p-1'>
+							{(item) => {
+								const {
+									label: title,
+									value: url,
+									metadata: { subtype: rawSubtype },
+								} = item;
 
-							const subtype = rawSubtype ?? 'page';
+								const subtype = rawSubtype ?? 'page';
 
-							let typeIcon = icons.file;
+								let typeIcon = icons.file;
 
-							if (subtype.toLowerCase() === 'url') {
-								typeIcon = icons.externalLink;
-							} else if (subtype.toLowerCase() === 'attachment') {
-								typeIcon = icons.file;
-							} else if (subtype.toLowerCase() === 'category') {
-								typeIcon = icons.layoutAlt;
-							} else if (subtype.toLowerCase() === 'internal') {
-								typeIcon = icons.anchor;
-							} else if (subtype.toLowerCase() === 'eightshift-forms') {
-								typeIcon = icons.formAlt;
-							}
-
-							if (suggestionTypeIconOverride) {
-								const overrideIcon = suggestionTypeIconOverride(subtype);
-
-								if (overrideIcon) {
-									typeIcon = overrideIcon;
+								if (subtype.toLowerCase() === 'url') {
+									typeIcon = icons.externalLink;
+								} else if (subtype.toLowerCase() === 'attachment') {
+									typeIcon = icons.file;
+								} else if (subtype.toLowerCase() === 'category') {
+									typeIcon = icons.layoutAlt;
+								} else if (subtype.toLowerCase() === 'internal') {
+									typeIcon = icons.anchor;
+								} else if (subtype.toLowerCase() === 'eightshift-forms') {
+									typeIcon = icons.formAlt;
 								}
-							}
 
-							return (
-								<Button
-									textValue={label}
-									size='large'
-									type='ghost'
-									className={clsx(
-										'es-uic-w-full es-uic-text-start',
-										'es-uic-grid es-uic-grid-cols-[auto,_minmax(0,_1fr)] es-uic-grid-rows-2 es-uic-items-center es-uic-justify-items-start es-uic-gap-x-2 es-uic-gap-y-0',
-										'[&>svg]:es-uic-row-span-2 [&>svg]:es-uic-shrink-0',
-										'[&>span:last-child]:es-uic-self-start [&>span]:es-uic-w-full [&>span]:es-uic-self-end [&>span]:es-uic-truncate',
-									)}
-									key={url}
-									onPress={() => {
-										setInputValue(url);
-										debounced(url);
+								if (suggestionTypeIconOverride) {
+									const overrideIcon = suggestionTypeIconOverride(subtype);
 
-										setSuggestionsVisible(false);
-									}}
-									icon={typeIcon}
-									autoFocus={index === 0}
-								>
-									<RichLabel
-										label={title}
-										subtitle={url?.replace(location.origin, '').replace(/\/$/, '')}
-										contentsOnly
-									/>
-								</Button>
-							);
-						})}
-					</Toolbar>
+									if (overrideIcon) {
+										typeIcon = overrideIcon;
+									}
+								}
+
+								return (
+									<ListBoxItem
+										id={item.value}
+										className={clsx(
+											'es-uic-rounded es-uic-p-1 es-uic-text-sm es-uic-transition',
+											'hover:es-uic-border-gray-300 hover:es-uic-bg-gray-100',
+											'focus-visible:es-uic-border-gray-300 focus-visible:es-uic-bg-gray-100',
+											'selected:es-uic-bg-teal-600/10 selected:es-uic-text-teal-900 selected:focus-visible:es-uic-bg-teal-600/15',
+										)}
+										textValue={url}
+									>
+										<RichLabel
+											icon={typeIcon}
+											label={title}
+											subtitle={url?.replace(location.origin, '').replace(/\/$/, '')}
+											noColor
+										/>
+									</ListBoxItem>
+								);
+							}}
+						</ListBox>
+
+						<Spacer border />
+
+						<div className='es-uic-grid es-uic-select-none es-uic-grid-cols-[auto,_1fr] es-uic-items-center es-uic-gap-x-1.5 es-uic-gap-y-1 es-uic-p-2 es-uic-text-sm es-uic-text-gray-500'>
+							<div className='es-uic-flex es-uic-gap-0.5 es-uic-justify-self-center'>
+								<kbd className='es-uic-flex es-uic-size-5 es-uic-items-center es-uic-justify-center es-uic-rounded es-uic-border es-uic-p-0.5 es-uic-font-sans es-uic-text-xs es-uic-text-gray-400'>
+									&darr;
+								</kbd>
+
+								<kbd className='es-uic-flex es-uic-size-5 es-uic-items-center es-uic-justify-center es-uic-rounded es-uic-border es-uic-p-0.5 es-uic-font-sans es-uic-text-xs es-uic-text-gray-400'>
+									&uarr;
+								</kbd>
+							</div>
+							{__('Navigate', 'eightshift-ui-components')}
+							<div className='es-uic-flex es-uic-gap-0.5 es-uic-justify-self-center'>
+								<kbd className='es-uic-flex es-uic-size-5 es-uic-items-center es-uic-justify-center es-uic-justify-self-center es-uic-rounded es-uic-border es-uic-p-0.5 es-uic-font-sans es-uic-text-xs es-uic-text-gray-400'>
+									&crarr;
+								</kbd>
+							</div>
+							{__('Select', 'eightshift-ui-components')}
+							<kbd className='es-uic-flex es-uic-h-5 es-uic-min-w-5 es-uic-items-center es-uic-justify-center es-uic-justify-self-center es-uic-rounded es-uic-border es-uic-p-0.5 es-uic-font-sans es-uic-text-xs es-uic-text-gray-400'>
+								Esc
+							</kbd>{' '}
+							{__('Hide suggestions', 'eightshift-ui-components')}
+						</div>
+					</>
 				)}
-
-				<Spacer
-					size='px'
-					border
-				/>
-
-				<div className='es-uic-grid es-uic-select-none es-uic-grid-cols-[auto,_1fr] es-uic-items-center es-uic-gap-x-1.5 es-uic-gap-y-1 es-uic-p-2 es-uic-text-sm es-uic-text-gray-500'>
-					<div className='es-uic-flex es-uic-gap-0.5 es-uic-justify-self-center'>
-						<kbd className='es-uic-flex es-uic-size-5 es-uic-items-center es-uic-justify-center es-uic-rounded es-uic-border es-uic-p-0.5 es-uic-font-sans es-uic-text-xs es-uic-text-gray-400'>
-							&uarr;
-						</kbd>
-						<kbd className='es-uic-flex es-uic-size-5 es-uic-items-center es-uic-justify-center es-uic-rounded es-uic-border es-uic-p-0.5 es-uic-font-sans es-uic-text-xs es-uic-text-gray-400'>
-							&darr;
-						</kbd>
-					</div>
-					{__('Navigate', 'eightshift-ui-components')}
-					<div className='es-uic-flex es-uic-gap-0.5 es-uic-justify-self-center'>
-						<kbd className='es-uic-flex es-uic-size-5 es-uic-items-center es-uic-justify-center es-uic-justify-self-center es-uic-rounded es-uic-border es-uic-p-0.5 es-uic-font-sans es-uic-text-xs es-uic-text-gray-400'>
-							&crarr;
-						</kbd>
-						<kbd className='es-uic-flex es-uic-size-5 es-uic-items-center es-uic-justify-center es-uic-justify-self-center es-uic-rounded es-uic-border es-uic-p-0.5 es-uic-font-sans es-uic-text-xs es-uic-text-gray-400'>
-							␣
-						</kbd>
-					</div>
-					{__('Select an item', 'eightshift-ui-components')}
-					<kbd className='es-uic-flex es-uic-h-5 es-uic-min-w-5 es-uic-items-center es-uic-justify-center es-uic-justify-self-center es-uic-rounded es-uic-border es-uic-p-0.5 es-uic-font-sans es-uic-text-xs es-uic-text-gray-400'>
-						Esc
-					</kbd>{' '}
-					{__('Close suggestion panel', 'eightshift-ui-components')}
-				</div>
 			</Popover>
-		</>
+		</ComboBox>
 	);
 };
