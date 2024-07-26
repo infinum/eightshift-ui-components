c5638142d22e0be1fd4c79f6510d7fa4625e97f7
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-07-26 17:06:47 +0200

    tweak linkinput

--- lib/components/link-input/link-input.jsx
+++ lib/components/link-input/link-input.jsx
@@ -168,9 +168,10 @@ export const LinkInput = (props) => {
 				aria-label={__('URL suggestions', 'eightshift-ui-components')}
 				className={({ isEntering, isExiting }) =>
 					clsx(
-						'es-uic-rounded-md es-uic-border es-uic-border-gray-200 es-uic-bg-white es-uic-shadow-lg es-uic-outline-none empty:es-uic-opacity-0',
+						'es-uic-rounded-md es-uic-border es-uic-border-gray-200 es-uic-bg-white es-uic-shadow-lg es-uic-outline-none',
 						isEntering && 'es-uic-outline es-uic-animate-in es-uic-fade-in-0 es-uic-slide-in-from-top-2 es-uic-fill-mode-forwards',
 						isExiting && 'es-uic-outline es-uic-animate-out es-uic-fade-out-0 es-uic-slide-out-to-top-2 es-uic-fill-mode-forwards',
+						!shouldShowSuggestions && suggestionList.items.length < 1 && 'es-uic-invisible',
 					)
 				}
 				style={{
