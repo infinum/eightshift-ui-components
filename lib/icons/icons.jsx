60daffef00607d129f7e9a68b20c25a115cf8599
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-11-28 20:17:41 +0100

    add loader icon

--- lib/icons/icons.jsx
+++ lib/icons/icons.jsx
@@ -22255,6 +22255,22 @@ export const icons = {
 			/>
 		</svg>
 	),
+	loader: (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='20'
+			height='20'
+			viewBox='0 0 20 20'
+			fill='none'
+		>
+			<path
+				d='M10 19v-2m-9-7h2m13.364 6.364L14.95 14.95M3.636 16.364 5.05 14.95M10 3V1m7 9h2M5.05 5.05 3.636 3.636M14.95 5.05l1.414-1.414'
+				stroke='currentColor'
+				stroke-linecap='round'
+				fill='none'
+			/>
+		</svg>
+	),
 };
 
 /**
