ab8002516834a934d8186c134c6964228da53bb5
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2024-12-17 14:32:07 +0100

    add new icons

--- lib/icons/icons.jsx
+++ lib/icons/icons.jsx
@@ -22271,6 +22271,90 @@ export const icons = {
 			/>
 		</svg>
 	),
+	verticalAlignBetween: (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			viewBox='0 0 20 20'
+			width='20'
+			height='20'
+			fill='none'
+		>
+			<path
+				d='m18.996 13.5-2 2m0 0-2-2m2 2v-11m2 2-2-2m0 0-2 2'
+				stroke='currentColor'
+				strokeLinecap='round'
+				strokeLinejoin='round'
+				fill='none'
+			/>
+			<path
+				d='M.996 1h18m-18 18h18'
+				stroke='currentColor'
+				strokeLinecap='round'
+				fill='none'
+			/>
+			<rect
+				x='9'
+				y='3'
+				width='4.5'
+				height='8'
+				rx='1.5'
+				transform='rotate(90 9 3)'
+				fill='currentColor'
+				fillOpacity='0.5'
+			/>
+			<rect
+				x='13'
+				y='12.5'
+				width='4.5'
+				height='12'
+				rx='1.5'
+				transform='rotate(90 13 12.5)'
+				fill='currentColor'
+				fillOpacity='0.5'
+			/>
+		</svg>
+	),
+	horizontalAlignBetween: (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			viewBox='0 0 20 20'
+			width='20'
+			height='20'
+			fill='none'
+		>
+			<path
+				d='m13.496 1 2 2m0 0-2 2m2-2h-11m2-2-2 2m0 0 2 2'
+				stroke='currentColor'
+				strokeLinecap='round'
+				strokeLinejoin='round'
+				fill='none'
+			/>
+			<path
+				d='M.996 19V1m18 18V1'
+				stroke='currentColor'
+				strokeLinecap='round'
+				fill='none'
+			/>
+			<rect
+				x='2.99609'
+				y='10.9961'
+				width='4.5'
+				height='8'
+				rx='1.5'
+				fill='currentColor'
+				fillOpacity='0.5'
+			/>
+			<rect
+				x='12.4961'
+				y='6.99609'
+				width='4.5'
+				height='12'
+				rx='1.5'
+				fill='currentColor'
+				fillOpacity='0.5'
+			/>
+		</svg>
+	),
 };
 
 /**
