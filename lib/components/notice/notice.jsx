2d0915baf3d75ece0842c20f77a477ddf4519586
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2025-07-14 13:56:38 +0200

    update notice styling

--- lib/components/notice/notice.jsx
+++ lib/components/notice/notice.jsx
@@ -33,45 +33,45 @@ export const Notice = (props) => {
 
 	const styles = {
 		info: {
-			icon: icons.infoCircle,
-			className: 'es:border-blue-400/75 es:shadow-blue-500/15 es:bg-blue-200/5',
-			iconColor: 'es:text-blue-400',
-			textColor: 'es:text-blue-500 es:saturate-75',
-			subtitleColor: 'es:text-blue-950/60',
+			icon: icons.info,
+			className: 'es:border-blue-500/75 es:shadow-blue-500/15 es:bg-blue-200/5 es:inset-ring-blue-100',
+			iconColorClassName: 'es:text-blue-500',
+			textColorClassName: 'es:text-blue-500 es:saturate-75',
+			subtitleColorClassName: 'es:text-blue-950/60',
 		},
 		success: {
 			icon: icons.checkSquare,
-			className: 'es:border-emerald-500/75 es:shadow-emerald-500/15 es:bg-emerald-200/5',
-			iconColor: 'es:text-emerald-500',
-			textColor: 'es:text-emerald-600',
-			subtitleColor: 'es:text-emerald-950/60',
+			className: 'es:border-green-600/75 es:shadow-green-500/15 es:bg-green-200/5 es:inset-ring-green-100',
+			iconColorClassName: 'es:text-green-600',
+			textColorClassName: 'es:text-green-700',
+			subtitleColorClassName: 'es:text-green-950/60',
 		},
 		warning: {
 			icon: icons.warning,
-			className: 'es:border-orange-500/75 es:shadow-orange-500/15 es:bg-orange-200/5',
-			iconColor: 'es:text-orange-500',
-			textColor: 'es:text-orange-700',
-			subtitleColor: 'es:text-orange-950/60',
+			className: 'es:border-orange-500/75 es:shadow-orange-500/15 es:bg-orange-200/5 es:inset-ring-orange-100',
+			iconColorClassName: 'es:text-orange-500',
+			textColorClassName: 'es:text-orange-700',
+			subtitleColorClassName: 'es:text-orange-950/60',
 		},
 		error: {
 			icon: icons.errorCircle,
-			className: 'es:border-red-300 es:shadow-red-500/15 es:bg-red-200/5',
-			iconColor: 'es:text-red-500',
-			textColor: 'es:text-red-700',
-			subtitleColor: 'es:text-red-950/60',
+			className: 'es:border-red-600/60 es:shadow-red-500/15 es:bg-red-200/5 es:inset-ring-red-100',
+			iconColorClassName: 'es:text-red-600',
+			textColorClassName: 'es:text-red-700',
+			subtitleColorClassName: 'es:text-red-900/70',
 		},
 		placeholder: {
 			icon: icons.componentGeneric,
-			className: 'es:border-secondary-400 es:shadow-secondary-300/25 es:border-dashed es:bg-secondary-200/5',
-			iconColor: 'es:text-secondary-400',
-			textColor: 'es:text-secondary-900',
-			subtitleColor: 'es:text-secondary-500',
+			className: 'es:border-indigo-600/60 es:shadow-indigo-500/15 es:bg-white es:outline-2 es:outline-white es:inset-ring-indigo-100/0 es:border-dashed',
+			iconColorClassName: 'es:text-indigo-600',
+			textColorClassName: 'es:text-indigo-700',
+			subtitleColorClassName: 'es:text-indigo-800/70',
 		},
 		default: {
-			className: 'es:border-secondary-300 es:shadow-secondary-300/25 es:bg-secondary-200/5',
-			iconColor: 'es:text-secondary-400',
-			textColor: 'es:text-secondary-900',
-			subtitleColor: 'es:text-secondary-500',
+			className: 'es:border-secondary-400 es:shadow-secondary-300/25 es:bg-secondary-200/5 es:inset-ring-secondary-100',
+			iconColorClassName: 'es:text-secondary-500',
+			textColorClassName: 'es:text-secondary-900',
+			subtitleColorClassName: 'es:text-secondary-500',
 		},
 	};
 
@@ -79,22 +79,22 @@ export const Notice = (props) => {
 		<div>
 			<div
 				className={clsx(
-					'es:grid es:grid-cols-[auto_1fr] es:grid-rows-[auto_auto] es:rounded-xl es:border es:bg-linear-to-tr es:shadow-sm',
+					'es:grid es:grid-cols-[auto_1fr] es:grid-rows-[auto_auto] es:rounded-xl es:border es:bg-linear-to-tr es:shadow-sm es:inset-ring-1',
 					styles[type].className,
-					icon || styles[type].icon ? 'es:gap-x-1.5 es:p-2' : 'es:px-2 es:py-2',
+					icon || styles[type].icon ? 'es:gap-x-1.5 es:p-2 es:pr-3 es:pl-2.5' : 'es:px-2.5 es:py-2',
 					className,
 				)}
 			>
 				{(icon || styles[type].icon) && (
-					<span
+					<div
 						className={clsx(
 							'es:col-span-1 es:col-start-1 es:row-span-2 es:row-start-1 es:shrink-0 es:icon:size-6',
 							alignIconToTitle ? 'es:self-baseline' : 'es:self-center',
-							styles[type].iconColor,
+							styles[type].iconColorClassName,
 						)}
 					>
 						{icon ?? styles[type].icon}
-					</span>
+					</div>
 				)}
 
 				{label && (
@@ -102,17 +102,18 @@ export const Notice = (props) => {
 						className={clsx(
 							'es:col-span-2 es:col-start-2 es:text-balance es:text-sm es:leading-tight',
 							subtitle ? 'es:self-end' : 'es:row-span-2 es:self-center',
-							styles[type].textColor,
+							styles[type].textColorClassName,
 						)}
 					>
 						{label}
 					</span>
 				)}
+
 				{subtitle && (
 					<span
 						className={clsx(
-							'es:col-span-2 es:col-start-2 es:text-balance es:text-xs es:leading-tight',
-							styles[type].subtitleColor,
+							'es:col-span-2 es:col-start-2 es:text-balance es:text-xs es:leading-tight es:pt-0.5',
+							styles[type].subtitleColorClassName,
 							label ? 'es:self-start' : 'es:row-span-2 es:self-center',
 						)}
 					>
