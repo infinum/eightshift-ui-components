9c38f1ac768abe06b269477e2d7772cfaee3ab4d
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2025-10-27 16:04:41 +0100

    tweak draggablehandle

--- lib/components/draggable/draggable-handle.jsx
+++ lib/components/draggable/draggable-handle.jsx
@@ -26,13 +26,14 @@ export const DraggableHandle = (props) => {
 	return (
 		<div
 			className={clsx(
-				'es:flex es:h-5 es:w-4 es:cursor-pointer es:items-center es:justify-center es:rounded es:border es:border-secondary-200 es:bg-white es:text-secondary-400 es:transition',
+				'es:flex es:h-5 es:w-4 es:cursor-pointer es:items-center es:justify-center es:rounded-sm es:hover:rounded-md es:inset-ring es:inset-ring-surface-400/10 es:bg-surface-50 es:text-surface-500/80 es:transition-plus',
+				'es:shadow-2xs es:shadow-surface-900/2',
 				'es:icon:size-4 es:icon:shrink-0',
-				'es:active:border-accent-500/30 es:active:bg-accent-50 es:active:text-accent-500',
+				'es:active:inset-ring-accent-500/30 es:active:bg-surface-100 es:active:text-accent-500 es:active:rounded-xl',
 				'es:any-focus:outline-hidden',
-				status !== 'dragging' && 'es:focus:border-accent-500 es:focus:ring-2 es:focus:ring-accent-500/50',
-				'es:hover:text-accent-500',
-				status === 'dragging' && 'es:border-accent-600! es:bg-accent-500! es:text-white! es:shadow-sm es:shadow-accent-500/30 es:ring-3 es:ring-accent-500/50',
+				status !== 'dragging' && 'es:focus:inset-ring-accent-500 es:focus:ring-2 es:focus:ring-accent-500/30',
+				'es:hover:text-accent-700',
+				status === 'dragging' && 'es:border-accent-600! es:bg-accent-500! es:text-white! es:shadow-sm es:shadow-black/5 es:ring-3 es:ring-accent-500/50',
 				className,
 			)}
 			ref={handleRef}
