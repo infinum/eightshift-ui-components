47e6ad8c6f833b2e3433e2390e4f7ec9be68dce5
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2026-01-16 11:03:31 +0100

    tweak popover background opacity and blur

--- lib/components/popover/popover.jsx
+++ lib/components/popover/popover.jsx
@@ -125,8 +125,8 @@ export const Popover = (props) => {
 			containerPadding={containerPadding}
 			className={({ isEntering, isExiting }) =>
 				clsx(
-					'es:rounded-2xl es:bg-linear-to-br es:from-surface-50/75 es:to-surface-50/75 es:inset-ring es:inset-ring-surface-500/15 es:shadow-xl es:outline-hidden',
-					'es:backdrop-blur-lg es:backdrop-brightness-110 es:backdrop-saturate-125',
+					'es:rounded-2xl es:bg-linear-to-br es:from-surface-50/85 es:to-surface-50/85 es:inset-ring es:inset-ring-surface-500/15 es:shadow-xl es:outline-hidden',
+					'es:backdrop-blur-xl es:backdrop-brightness-110 es:backdrop-saturate-125',
 					'es:inset-shadow-xs es:inset-shadow-white/50',
 					'es:placement-bottom:origin-top-left es:placement-top:origin-bottom-left',
 					'es:placement-left:origin-right es:placement-right:origin-left',
