f293f4ab5c8dd927bb776eccc3d83fcb613ec650
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2026-02-13 14:08:22 +0100

    remove duplicate icons

--- lib/icons/icons.jsx
+++ lib/icons/icons.jsx
@@ -5247,13 +5247,6 @@ export const icons = {
 			<path d='M1 1h18M1 19h18' stroke='currentColor' strokeLinecap='round' fill='none' />
 		</svg>
 	),
-	verticalAlignBetween: (
-		<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' width='20' height='20' fill='none'>
-			<rect x='0.75' y='3' width='4.5' height='14' rx='1.5' fill='currentColor' fillOpacity='0.5' />
-			<rect x='14.75' y='3' width='4.5' height='14' rx='1.5' fill='currentColor' fillOpacity='0.5' />
-			<path d='M1 1h18M1 19h18' stroke='currentColor' strokeLinecap='round' fill='none' />
-		</svg>
-	),
 	horizontalAlignAround: (
 		<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' width='20' height='20' fill='none'>
 			<rect width='4.5' height='14' rx='1.5' transform='matrix(0 1 1 0 3 2.75)' fill='currentColor' fillOpacity='0.5' />
@@ -5265,13 +5258,6 @@ export const icons = {
 		<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' width='20' height='20' fill='none'>
 			<rect width='4.5' height='14' rx='1.5' transform='matrix(0 1 1 0 3 3.75)' fill='currentColor' fillOpacity='0.5' />
 			<rect width='4.5' height='14' rx='1.5' transform='matrix(0 1 1 0 3 11.75)' fill='currentColor' fillOpacity='0.5' />
-			<path d='M1 1v18M19 1v18' stroke='currentColor' strokeLinecap='round' fill='none' />
-		</svg>
-	),
-	horizontalAlignBetween: (
-		<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' width='20' height='20' fill='none'>
-			<rect width='4.5' height='14' rx='1.5' transform='matrix(0 1 1 0 3 .75)' fill='currentColor' fillOpacity='0.5' />
-			<rect width='4.5' height='14' rx='1.5' transform='matrix(0 1 1 0 3 14.75)' fill='currentColor' fillOpacity='0.5' />
 			<path d='M1 1v18M19 1v18' stroke='currentColor' strokeLinecap='round' fill='none' />
 		</svg>
 	),
