aeccfeb4adca4976ea5a88034f26a0b08f980cd7
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2026-05-29 16:11:26 +0200

    fix missing font imports for demo app

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import '../font-faces/geist.css';
+import '../font-faces/geist-mono.css';
+import '../font-faces/google-sans-flex.css';
 import '../lib/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
