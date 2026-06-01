3f82e2344f8e17f5bc1b6d5479e2f57ad25c83dc
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2026-06-01 15:57:26 +0200

    add help-alt icon

---
+++ lib/icons/ui-icons/help-alt.tsx
@@ -0,0 +1,23 @@
+export const helpAlt = (
+	<svg
+		aria-hidden='true'
+		focusable='false'
+		width='20'
+		height='20'
+		viewBox='0 0 20 20'
+		fill='none'
+		xmlns='http://www.w3.org/2000/svg'
+	>
+		<path
+			d='M6.5 6.5C6.5 5.43939 7.2 3 10 3C12.8 3 13.5 5.09091 13.5 6C13.5 8.27273 11.767 8.73297 10.5 10C9.5 11 9.5 12 9.5 13'
+			stroke='currentColor'
+			strokeLinecap='round'
+		/>
+		<circle
+			cx='9.5'
+			cy='16.5'
+			r='1'
+			fill='currentColor'
+		/>
+	</svg>
+);
