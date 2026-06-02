67abc5a24f83bbdf43206e7a8ae36342e1b25659
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2026-06-02 11:21:57 +0200

    update icons

--- lib/icons/ui-icons/help.tsx
+++ lib/icons/ui-icons/help.tsx
@@ -2,34 +2,31 @@ export const help = (
 	<svg
 		aria-hidden='true'
 		focusable='false'
-		xmlns='http://www.w3.org/2000/svg'
-		viewBox='0 0 20 20'
 		width='20'
 		height='20'
+		viewBox='0 0 20 20'
 		fill='none'
+		xmlns='http://www.w3.org/2000/svg'
 	>
 		<circle
 			cx='10'
 			cy='10'
-			r='9'
+			r='9.5'
 			stroke='currentColor'
 			strokeOpacity='0.3'
-			fill='none'
-		/>
-		<circle
-			cx='10'
-			cy='14.75'
-			r='0.5'
-			fill='currentColor'
-			stroke='currentColor'
-			strokeWidth='0.5'
 		/>
 		<path
-			d='M7.5 7.474c.114-.866 1.317-2.599 3.533-1.737 1.883.732 1.256 3.029.288 4.112-1.056 1.181-1.467 1.42-1.381 2.654'
+			d='M6.56 7.46311C6.56 6.2582 7.72475 4.25 9.97393 4.25C12.2231 4.25 13.1368 5.85656 13.2875 7.06148C13.4381 8.26639 12.9862 9.06967 11.7813 10.1742C10.1924 11.6307 9.97393 11.8811 9.97393 13.0861'
 			stroke='currentColor'
 			strokeLinecap='round'
-			strokeLinejoin='round'
-			fill='none'
+		/>
+		<rect
+			x='9.25'
+			y='15.25'
+			width='1.5'
+			height='1.5'
+			rx='0.75'
+			fill='currentColor'
 		/>
 	</svg>
 );
